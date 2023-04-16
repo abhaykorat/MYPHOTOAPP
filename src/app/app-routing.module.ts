547919b03d9c8dcfa79b2fa41f10dc8eaf7f216b
Abhay <abhay.korat.3@gmail.com>
2023-04-16 06:13:10 -0400

    components added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ProfileComponent } from './profile/profile.component';
+import { LoginComponent } from './login/login.component';
+import { AlbumDetailsComponent } from './album-details/album-details.component';
+import { CreateAlbumComponent } from './create-album/create-album.component';
+import { MyAlbumsComponent } from './my-albums/my-albums.component';
+import { PhotoDetailsComponent } from './photo-details/photo-details.component';
+import { UploadPictureComponent } from './upload-picture/upload-picture.component';
 
-const routes: Routes = [{ path: 'profilecomp', component: ProfileComponent}];
+const routes: Routes = [
+{ path: 'profile', component: ProfileComponent},
+{ path: 'login', component: LoginComponent},
+{ path: 'albums', component: MyAlbumsComponent},
+{ path: 'create', component: CreateAlbumComponent},
+{ path: 'album', component: AlbumDetailsComponent},
+{ path: 'upload', component: UploadPictureComponent},
+{ path: 'photo', component: PhotoDetailsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
