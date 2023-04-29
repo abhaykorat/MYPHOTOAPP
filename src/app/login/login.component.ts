b40561a0b696726fcc78cba4bd94f3f5cb9b7859
Abhay <abhay.korat.3@gmail.com>
2023-04-29 02:03:06 -0400

    no message

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+
+  signInFormVisible = true;
+
+
+  constructor() {}
+
+  ngOnInit(): void {
+  }
+
+  makeSignInFormVisible(){
+    this.signInFormVisible = true;
+  }
+
+  makeSignUpFormVisible(){
+    this.signInFormVisible = false;
+  }
+
+ 
 
 }
